2a411d1a423f73fef2221dd9168466573bb8a0ab
juliaandreis <juliarenata19@gmail.com>
2025-04-23 06:52:00 -0300

    Create Combate.h

---
+++ POO_TrabalhoGA/Combate.h
@@ -0,0 +1,19 @@
+#ifndef COMBATE_H
+#define COMBATE_H
+
+#include <string>
+#include "Personagem.h"
+
+using namespace std;
+
+class Combate{
+    private:
+        Personagem* personagem;
+
+    public:
+        Combate();
+        ~Combate();
+
+};
+
+#endif // COMBATE_CPP
