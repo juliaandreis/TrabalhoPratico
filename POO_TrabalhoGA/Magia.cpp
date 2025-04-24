968054670233cf2ddfbc232af07beba6938f5770
juliaandreis <juliarenata19@gmail.com>
2025-04-24 00:54:09 -0300

    Update Magia.cpp

--- POO_TrabalhoGA/Magia.cpp
+++ POO_TrabalhoGA/Magia.cpp
@@ -3,3 +3,25 @@
 #include "Magia.h"
 
 using namespace std;
+
+Magia::Magia(){}
+
+Magia::~Magia(){}
+
+Magia::Magia(string nome, int dano, bool item_magico){
+    this->nome = nome;
+    this->dano = dano;
+    this->item_magico = item_magico;
+}
+
+void Magia::setNome(string nome){this->nome = nome;}
+
+string Magia::getNome(){return nome;}
+
+void Magia::setDano(int dano){this->dano = dano;}
+
+int Magia::getDano(){return dano;}
+
+void Magia::setItem_magico(bool item_magico){this->item_magico = item_magico;}
+
+bool Magia::getItem_magico(){return item_magico;}
