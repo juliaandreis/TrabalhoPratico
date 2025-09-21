b5f9a4ed9581b0dd9db2793cf18825c107dcb06e
juliaandreis <juliarenata19@gmail.com>
2025-09-21 19:08:11 -0300

    Update client.py

--- Redes/client.py
+++ Redes/client.py
@@ -1,32 +1,62 @@
+#julia andreis e tobias klein
+
 import socket
+import threading
+import time
+
+# Configurações do cliente
+HOST = '192.168.1.108' # IP do servidor
+PORT = 50000           # Porta do servidor
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(2048).decode('utf-8')
+            if message:
+                print("\r" + message + "\n> ", end="")
+            else:
+                break
+        except:
+            print("Conexão encerrada pelo servidor.")
+            client_socket.close()
+            break
+        
+def send_messages(client_socket): 
+    while True:
+        try:
+            message = input("> ")
+            if message.lower() == "/sair":
+                break
+            client_socket.send(message.encode('utf-8'))
+        except:
+            print("Erro ao enviar mensagem.")
+            break
+    client_socket.close()
 
 
-def run_client():
-    # create a socket object
+def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((HOST, PORT))
+    except:
+        return print('\nNão foi possível se conectar ao servidor!\n')
 
-    server_ip = "127.0.0.1"  # replace with the server's IP address
-    server_port = 8000  # replace with the server's port number
-    # establish connection with server
-    client.connect((server_ip, server_port))
+    username = input('Digite o seu usuário > ')
+    client.send(username.encode('utf-8'))
+    
+    thread_receive = threading.Thread(target=receive_messages, args=(client,), daemon=True)
+    thread_receive.start()
+
+    thread_send = threading.Thread(target=send_messages, args=(client,), daemon=True)
+    thread_send.start()
 
     while True:
-        # input message and send it to the server
-        msg = input("Enter message: ")
-        client.send(msg.encode("utf-8")[:1024])
-
-        # receive message from the server
-        response = client.recv(1024)
-        response = response.decode("utf-8")
-
-        # if server sent us "closed" in the payload, we break out of the loop and close our socket
-        if response.lower() == "closed":
+        if not thread_receive.is_alive() or not thread_send.is_alive():
             break
-
-        print(f"Received: {response}")
-
-    # close client socket (connection to the server)
+        time.sleep(1) # Impede o loop de consumir 100% da CPU
+    
     client.close()
-    print("Connection to server closed")
+    print("Você foi desconectado.")
 
-run_client()
+if __name__ == "__main__":
+    start_client()
